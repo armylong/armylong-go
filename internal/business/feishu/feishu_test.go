4c783810b1293d686b1a1b6e03c58f749d151612
armylong <armylong@163.com>
2026-03-29 15:55:55 +0800

    tables

--- internal/business/feishu/feishu_test.go
+++ internal/business/feishu/feishu_test.go
@@ -5,17 +5,10 @@ import (
 	"fmt"
 	"testing"
 
-	feishuCs "github.com/armylong/armylong-go/internal/cs/feishu"
 	feishuLibrary "github.com/armylong/go-library/service/feishu"
 )
 
 var ctx = context.Background()
-
-func TestGetAuthorizationHeader(t *testing.T) {
-	BitableBusiness.GetBitable(ctx, &feishuCs.BitableRequest{
-		AppToken: "6BHkHf8DJAbdA608F9azx89x2xK9LHwd",
-	})
-}
 
 func TestInitUserAccessToken(t *testing.T) {
 	code := "bDKkDf7Kyy88Ab18Hfz7KyI20ybzbH4K"
